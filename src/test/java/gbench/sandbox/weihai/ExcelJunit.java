7bb71592996fae2fdb6e86fb808872c0b4850dbb
gbench <gbench@sina.com>
2022-05-31 11:45:50 +0800

    格式化

--- src/test/java/gbench/sandbox/weihai/ExcelJunit.java
+++ src/test/java/gbench/sandbox/weihai/ExcelJunit.java
@@ -15,7 +15,7 @@ public class ExcelJunit {
     @Test
     public void foo() {
         final SimpleExcel excel = SimpleExcel.of("C:\\Users\\xuqinghua\\Desktop\\项目配置.xlsx");
-        excel.sheets().forEach(e->{
+        excel.sheets().forEach(e -> {
             println(excel.autoDetect(e));
         });
         final StrMatrix mx = excel.autoDetect(0);
@@ -23,7 +23,7 @@ public class ExcelJunit {
         println(mx.rows().get(0));
         println(mx);
         println(mx.transpose());
-        println(mx.colS(e->e.map2(Arrays::asList)).collect(IRecord.recclc()));
+        println(mx.colS(e -> e.map2(Arrays::asList)).collect(IRecord.recclc()));
         excel.close();
     }
 
