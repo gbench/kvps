74f23c4c3d9f9f0453509576a79ce1fa804129dc
gbench <gbench@sina.com>
2022-06-09 08:59:47 +0800

    增加需求与人员测试模块

---
+++ src/test/java/gbench/sandbox/weihai/devops/EmployeeJunit.java
@@ -0,0 +1,43 @@
+package gbench.sandbox.weihai.devops;
+
+import java.util.Map;
+import java.util.List;
+
+import static gbench.util.io.Output.println;
+import static gbench.util.lisp.IRecord.FT;
+import static gbench.util.lisp.IRecord.REC;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import gbench.util.lisp.DFrame;
+import gbench.util.lisp.IRecord;
+import gbench.util.lisp.Tuple2;
+
+public class EmployeeJunit extends DevOpsClient{
+    /**
+     * 项目创建
+     * 
+     * @param params
+     * @return
+     */
+    public IRecord create(final IRecord params) {
+        final String api = apiOf("/auth/user/sys/bulkinsert");
+        return this.post_json(api, params);
+    }
+    
+    @Test
+    public void foo_create() {
+        final List<Map<String, Object>> userListVO = Arrays.stream("zhangsan,lisi,wangwu,zhaoliu".split("[,]+"))
+                .map(Tuple2.snb(0)).map(e -> REC( //
+                        "email", FT("$0@gazellio.com", e._2), "realName", e._2, //
+                        "telephone", "123598712" + e._1, "username", "cs_" + e._2, //
+                        "role", "outsourceDeveloper"))
+                .map(e -> e.toMap2()).collect(Collectors.toList());
+        final IRecord params = REC("userListVO",userListVO,"serviceProvider","上海速邦信息科技有限公司","projectOid",oid());
+        println(params.llS2("userListVO", IRecord::REC).collect(DFrame.dfmclc));
+        this.create(params);
+    }
+}
---
+++ src/test/java/gbench/sandbox/weihai/devops/ReqJunit.java
@@ -0,0 +1,59 @@
+package gbench.sandbox.weihai.devops;
+
+import static gbench.util.lisp.IRecord.REC;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import gbench.util.lisp.IRecord;
+
+/**
+ * 
+ * @author xuqinghua
+ *
+ */
+public class ReqJunit extends DevOpsClient {
+    /**
+     * 项目创建
+     * 
+     * @param params
+     * @return
+     */
+    public IRecord create(final IRecord params) {
+        final String api = apiOf("/auth/oa/businessSheet/insert");
+        return this.post_json(api, params);
+    }
+
+    @Test
+    public void foo_create() {
+        final LocalDate startDate = LocalDate.now();
+        final IRecord daily_params = REC( //
+                "approvalNodes", this.approvalNodes(null) //
+                , "assistSystemOidList", Arrays.asList(oid(), oid(), oid()) // 协办系统
+                , "endDate", startDate.plusDays(10) //
+                , "fileOaUrl", "" //
+                , "hostSystemOid", oid() // 主办系统
+//                ,"hostSystemProjectOid",oid() // 日常需求
+                , "ifNew", true //
+                , "oid", oid() // 需求id
+                , "startDate", startDate //
+                , "title", "日常需求" //
+        );
+
+        final IRecord proj_params = REC( //
+                "approvalNodes", this.approvalNodes(null) //
+//                , "assistSystemOidList", Arrays.asList(oid(), oid(), oid()) // 协办系统
+//                , "endDate", startDate.plusDays(10) //
+                , "fileOaUrl", "" //
+//                , "hostSystemOid", oid() // 主办系统
+                , "hostSystemProjectOid", oid() // 日常需求
+//                , "ifNew", true //
+//                , "oid", oid() // 需求id
+//                , "startDate", startDate //
+//                , "title", "日常需求" //
+        );
+
+        this.create(daily_params);
+        this.create(proj_params);
+    }
+}
