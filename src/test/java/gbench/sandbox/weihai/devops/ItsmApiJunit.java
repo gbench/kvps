809986206277ac1cf28776ed67f20824a990da5d
gbench <gbench@sina.com>
2022-06-09 13:25:14 +0800

    变量重命名

--- src/test/java/gbench/sandbox/weihai/devops/ItsmApiJunit.java
+++ src/test/java/gbench/sandbox/weihai/devops/ItsmApiJunit.java
@@ -12,6 +12,18 @@ import com.alibaba.fastjson.JSONObject;
 public class ItsmApiJunit extends ItsmClient {
 
     /**
+     * 获取首页配置信息
+     * 
+     * @return {loginLogo: 登陆页面Logo地址,indexLogo: 首页面Logo地址,loginBGImage:
+     *         登陆页面背景图片地址,emailAddress: 快速提交邮箱地址}
+     */
+    public JSONObject getIndexConfigMessage() {
+        final String api = apiOf("/SysConfig/getIndexConfigMessage");
+        final JSONObject json = httpPost2(api, JREC(), this.token());
+        return json;
+    }
+
+    /**
      * 登录演示
      */
     @Test
@@ -32,10 +44,8 @@ public class ItsmApiJunit extends ItsmClient {
      * token api 调用演练
      */
     @Test
-    public void foo_api() {
-        final String api = apiOf("basicsApi/itsmBasicsApi/SysConfig/getIndexConfigMessage");
-        final JSONObject json = send_post2(api, JREC(), this.token());
-        System.out.println(json);
+    public void foo_api_demo() {
+        System.out.println(this.getIndexConfigMessage());
     }
 
 }
--- src/test/java/gbench/sandbox/weihai/devops/ItsmClient.java
+++ src/test/java/gbench/sandbox/weihai/devops/ItsmClient.java
@@ -20,37 +20,40 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import gbench.sandbox.weihai.devops.util.Base64Util;
-import gbench.util.lisp.IRecord;
-import gbench.util.lisp.MyRecord;
 
 /**
+ * ItsmClient 客户端 示例 <br>
  * 
- * @author xuqinghua
+ * url: https://whbank.gazellio.com/apiIndex <br>
+ * 快速查询: 1.45.56 用户登录 username: sys_admin <br>
+ * password: super123456 <br>
+ * 
+ * @author gbench
  *
  */
 public class ItsmClient {
 
     /**
-     * url https://whbank.gazellio.com/apiIndex <br>
-     * account: sys_admin <br>
-     * password: super123456 <br>
+     * 用户登录
      * 
      * @param name     账号
      * @param password 密码
      */
-    public String login(String name, String password) {
-        final String url = apiOf("/basicsApi/itsmBasicsApi/user/loginCheckUser");
-        System.err.println(url);
-        JSONObject object = new JSONObject();
+    public String login(final String name, final String password) {
+        final String url = apiOf("/user/loginCheckUser");
+
+        // System.err.println(url);
+
+        final JSONObject object = new JSONObject();
         object.put("username", name);
         object.put("password", password);
-        String result = ItsmClient.sendPost(url, object, null);
-        String res = Base64Util.unescape(result);
-        return res;
+        final String result = httpPost(url, object, null);
+
+        return result;
     }
 
     /**
-     * 获取请求token
+     * 获取访问token
      * 
      * @return token
      */
@@ -59,14 +62,15 @@ public class ItsmClient {
     }
 
     /**
+     * 提取范文token
      * 
-     * @param user
-     * @param password
-     * @return
+     * @param user     用户名
+     * @param password 密码
+     * @return 访问token
      */
-    public String token(String user, String password) {
-        final String _u = Optional.ofNullable(user).orElse(ROOT_USER);
-        final String _p = Optional.ofNullable(password).orElse(ROOT_PASSWORD);
+    public String token(final String user, final String password) {
+        final String _u = Optional.ofNullable(user).orElse(DEFAULT_USERNAME);
+        final String _p = Optional.ofNullable(password).orElse(DEFAULT_PASSWORD);
         final String line = this.login(_u, _p);
 
         String token = null;
@@ -76,44 +80,90 @@ public class ItsmClient {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return token;
     }
 
     /**
+     * MAP 键值对儿生成器
      * 
-     * @param line
+     * @param kvs 键,值序列:key0,value0,key1,value1,.... <br>
+     *            Map结构（IRecord也是Map结构） 或是 键名,键值 序列。即 build(map) 或是
+     *            build(key0,value0,key1,vlaue1,...) 的 形式， 特别注意 build(map) 时候，当且仅当
+     *            kvs 的只有一个元素，即 build(map0,map1) 会被视为 键值序列
      * @return
      */
-    @SuppressWarnings("unchecked")
-    public static Map<String, Object> json2map(String line) {
-        final Map<String, Object> json = new LinkedHashMap<String, Object>();
-        json.putAll((Map<String, Object>) JSONObject.parse(line));
-        return json;
+    public static JSONObject JREC(final Object... kvs) {
+        return JSONObject.parseObject(toJson(REC(kvs)));
+    }
+
+    /**
+     * MAP 键值对儿生成器
+     * 
+     * @param kvs 键,值序列:key0,value0,key1,value1,.... <br>
+     *            Map结构（IRecord也是Map结构） 或是 键名,键值 序列。即 build(map) 或是
+     *            build(key0,value0,key1,vlaue1,...) 的 形式， 特别注意 build(map) 时候，当且仅当
+     *            kvs 的只有一个元素，即 build(map0,map1) 会被视为 键值序列
+     * @return
+     */
+    public static Map<String, Object> REC(final Object... kvs) {
+        final int n = kvs.length;
+        final LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>();
+
+        if (n == 1) { // 单一参数情况
+            final Object obj = kvs[0];
+            if (obj instanceof Map) { // Map情况的数据处理
+                ((Map<?, ?>) obj).forEach((k, v) -> { // 键值的处理
+                    data.put(k + "", v);
+                }); // forEach
+            } else if (obj instanceof String) { // 字符串类型的单个参数
+                final Map<String, Object> jsonMap = parseJson(obj.toString()); // 尝试解析json
+                if (jsonMap != null) {
+                    data.putAll(jsonMap);
+                }
+            } // if
+        } else { // 键名减值序列
+            for (int i = 0; i < n - 1; i += 2) {
+                data.put(kvs[i].toString(), kvs[i + 1]);
+            }
+        } // if
+        return data;
+    }
+
+    /**
+     * 解析JSON
+     * 
+     * @param jsonString json 字符串
+     * @return Map 对象
+     */
+    public static Map<String, Object> fromJson(final String jsonString) {
+        return parseJson(jsonString);
     }
 
     /**
      * 
-     * @param line
-     * @return
-     */
-    public static JSONObject parseJson(String line) {
-        return JSONObject.parseObject(line);
-    }
-
-    /**
-     * 
-     * @param obj
-     * @return
+     * @param obj jsonObject 对象
+     * @return json 字符串
      */
     public static String toJson(final Object obj) {
         return JSONObject.toJSONString(obj, false);
     }
 
     /**
+     * 解析JSON
+     * 
+     * @param jsonString json 字符串
+     * @return JSONObject
+     */
+    public static JSONObject parseJson(final String jsonString) {
+        return JSONObject.parseObject(jsonString);
+    }
+
+    /**
      * 格式化
      * 
-     * @param obj
-     * @return
+     * @param jsonString
+     * @return json 格式化字符串
      */
     public static String pretty(final String jsonString) {
         JSONObject object = JSONObject.parseObject(jsonString);
@@ -135,7 +185,8 @@ public class ItsmClient {
         HttpPost post = new HttpPost(url);
         post.setHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
         post.addHeader("token", token);
-        String result;
+        String result = null;
+
         try {
             UrlEncodedFormEntity s = new UrlEncodedFormEntity(
                     Arrays.asList(new BasicNameValuePair("info", Base64Util.escape(json.toJSONString()))));
@@ -157,8 +208,9 @@ public class ItsmClient {
                 System.out.println("请求服务端失败");
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
+
         return result;
     }
 
@@ -166,69 +218,21 @@ public class ItsmClient {
      * 生成api路径
      * 
      * @param path api 项目路径
-     * @return
+     * @return api路径
      */
     public static String apiOf(final String path) {
         final String sep = path.startsWith("/") ? "" : "/";
-        return HOST + sep + path;
-    }
-
-    /**
-     * MAP 键值对儿生成器
-     * 
-     * @param kvs 键,值序列:key0,value0,key1,value1,.... <br>
-     *            Map结构（IRecord也是Map结构） 或是 键名,键值 序列。即 build(map) 或是
-     *            build(key0,value0,key1,vlaue1,...) 的 形式， 特别注意 build(map) 时候，当且仅当
-     *            kvs 的只有一个元素，即 build(map0,map1) 会被视为 键值序列
-     * @return
-     */
-    public static JSONObject JREC(Object... kvs) {
-        return JSONObject.parseObject(toJson(REC(kvs)));
-    }
-
-    /**
-     * MAP 键值对儿生成器
-     * 
-     * @param kvs 键,值序列:key0,value0,key1,value1,.... <br>
-     *            Map结构（IRecord也是Map结构） 或是 键名,键值 序列。即 build(map) 或是
-     *            build(key0,value0,key1,vlaue1,...) 的 形式， 特别注意 build(map) 时候，当且仅当
-     *            kvs 的只有一个元素，即 build(map0,map1) 会被视为 键值序列
-     * @return
-     */
-    public static Map<String, Object> REC(Object... kvs) {
-        final int n = kvs.length;
-        final LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>();
-
-        if (n == 1) { // 单一参数情况
-            final Object obj = kvs[0];
-            if (obj instanceof Map) { // Map情况的数据处理
-                ((Map<?, ?>) obj).forEach((k, v) -> { // 键值的处理
-                    data.put(k + "", v);
-                }); // forEach
-            } else if (obj instanceof IRecord) {// IRecord 对象类型 复制对象数据
-                data.putAll(((IRecord) obj).toMap());
-            } else if (obj instanceof String) { // 字符串类型的单个参数
-                final IRecord rec = MyRecord.fromJson(obj.toString()); // 尝试解析json
-                if (rec != null) {
-                    data.putAll(rec.toMap());
-                }
-            } // if
-        } else { // 键名减值序列
-            for (int i = 0; i < n - 1; i += 2) {
-                data.put(kvs[i].toString(), kvs[i + 1]);
-            }
-        } // if
-        return data;
+        return API_SERVER_URL_PREFIX + sep + path;
     }
 
     /**
      * 发送post请求,带有解析翻译的
      * 
-     * @param json
-     * @param url
-     * @return
+     * @param json 请求参数
+     * @param url  请求地址
+     * @return json 字符串
      */
-    public static String send_post(String url, JSONObject json, String token) {
+    public static String httpPost(String url, JSONObject json, String token) {
         final String line = sendPost(url, json, token);
         return Base64Util.unescape(line);
     }
@@ -236,16 +240,26 @@ public class ItsmClient {
     /**
      * 发送post请求,带有解析翻译的
      * 
-     * @param json
-     * @param url
-     * @return
+     * @param json 请求参数
+     * @param url  请求地址
+     * @return json 字符串
      */
-    public static JSONObject send_post2(String url, JSONObject json, String token) {
-        return JSONObject.parseObject(send_post(url, json, token));
+    public static JSONObject httpPost2(String url, JSONObject json, String token) {
+        return JSONObject.parseObject(httpPost(url, json, token));
     }
 
-    public static String ROOT_USER = "sys_admin";
-    public static String ROOT_PASSWORD = "super123456";
-    public static String HOST = "https://whbank.gazellio.com";
+    /**
+     * 默认用户名
+     */
+    public static String DEFAULT_USERNAME = "sys_admin";
+    /**
+     * 默认密码
+     */
+    public static String DEFAULT_PASSWORD = "super123456";
+
+    /**
+     * API 服务器接口地址前缀
+     */
+    public static String API_SERVER_URL_PREFIX = "https://whbank.gazellio.com/basicsApi/itsmBasicsApi";
 
 }
