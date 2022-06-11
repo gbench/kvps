9b5d0615b812e05bcd0997c88513c85ff3365f36
gbench <gbench@sina.com>
2022-06-11 23:12:11 +0800

    优化代码

--- src/main/java/gbench/whccb/kvps/config/JacksonConfig.java
+++ src/main/java/gbench/whccb/kvps/config/JacksonConfig.java
@@ -1,25 +1,11 @@
 package gbench.whccb.kvps.config;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.std.DateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
-import gbench.util.json.jackson.IRecordModule;
+import gbench.util.json.MyJson;
 
 /**
  * 自定义jackson配置 <br>
@@ -31,28 +17,6 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-//        // 序列换成json时,将所有的long变成string
-//        final SimpleModule lng2strModule = new SimpleModule();
-//        lng2strModule.addSerializer(Long.class, ToStringSerializer.instance);
-//        lng2strModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-
-        // 日期序列化设置
-        final JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(Date.class,
-                new DateSerializer(false, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")));
-        javaTimeModule.addSerializer(LocalDateTime.class,
-                new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        javaTimeModule.addSerializer(LocalDate.class,
-                new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
-
-        // IRecord 序列化
-        final IRecordModule recordModule = new IRecordModule();
-        final List<com.fasterxml.jackson.databind.Module> modules = Arrays.asList(javaTimeModule, recordModule);
-        final ObjectMapper objM = new ObjectMapper();
-
-        modules.forEach(objM::registerModule); // 注册模块
-
-        return objM;
+        return MyJson.recM(); // 默认的objectMapper，具有 时间 与 IRecord 解析能力
     }
 }
